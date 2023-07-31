cb4143711ae294b3263705637205580e8b231549
Nagy Richard <herold.humor99@gmail.com>
2023-07-31 15:15:33 +0200

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+
+        public int Id { get; set; }
+        public string JokeQuesiton  { get; set; }
+        public string JokeAnswer{ get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
